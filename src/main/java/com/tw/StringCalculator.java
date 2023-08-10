5367c567d757ade7d07d5b901611faa73559679b
Jens Jovellano <jensjovellano@gmail.com>
2023-08-10 10:50:06 +0800

    String calculator code

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,18 @@
 package com.tw;
 
+import java.util.ArrayList;
+
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        if (string.isEmpty()) return 0;
+        int result = 0;
+        ArrayList<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isDigit(string.charAt(i))) nums.add(Character.getNumericValue(string.charAt(i)));
+        }
+        for (int num : nums) {
+            result += num;
+        }
+        return result;
     }
 }
